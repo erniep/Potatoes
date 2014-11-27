63e1aa68825e334c1c6ab6a434766c74a623c49d
Vbrzeski <Vbrzeski@gmail.com>
2014-11-26 18:45:20 -0600

    Motors0.3
    Motors .c, .h, files. Ready for light testing. 2 Functions to be filled
    later on

--- motors.c
+++ motors.c
@@ -4,73 +4,7 @@
  *  Created on: Nov 26, 2014
  *      Author: Wicho
  */
-/* Standard Libraries */
-//#include <stdlib.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-/* TIVAC Function Library */
-#include <driverlib/pin_map.h>
-#include <inc/hw_memmap.h>
-#include <driverlib/pwm.h>
-#include <driverlib/gpio.h>
-#include <driverlib/sysctl.h>
-
-
-// Switching frequencies
-#define swfreq_motors 	50
-#define swfreq_servos 	30
-// PWM Clock Prescaler
-
-// PWM out Pins
-#define M1_OUT			PWM_OUT_2
-#define M2_OUT			PWM_OUT_3
-#define M3_OUT			PWM_OUT_0
-#define M4_OUT			PWM_OUT_1
-#define SERVO1_OUT		PWM_OUT_4
-#define SERVO2_OUT		PWM_OUT_5
-#define M1_OUT_BIT		PWM_OUT_2_BIT
-#define M2_OUT_BIT		PWM_OUT_3_BIT
-#define M3_OUT_BIT		PWM_OUT_0_BIT
-#define M4_OUT_BIT		PWM_OUT_1_BIT
-#define SERVO1_OUT_BIT	PWM_OUT_4_BIT
-#define SERVO2_OUT_BIT	PWM_OUT_5_BIT
-// PWM Gens
-#define PWM_MOTOR_BASE	PWM1_BASE
-#define PWM_SERVO_BASE	PWM0_BASE
-#define PWM_GEN_BOTM	PWM_GEN_0
-#define PWM_GEN_TOPM	PWM_GEN_1
-#define PWM_GEN_SERVO	PWM_GEN_2
-// GPIO Defines - Motor
-#define GPIO_M1 		GPIO_PIN_6
-#define GPIO_M2 		GPIO_PIN_7
-#define GPIO_M3 		GPIO_PIN_0
-#define GPIO_M4 		GPIO_PIN_1
-#define GPIO_SERVO1 	GPIO_PIN_4
-#define GPIO_SERVO2 	GPIO_PIN_5
-#define GPIO_BASE_TOPM	GPIO_PORTA_BASE
-#define GPIO_BASE_BOTM	GPIO_PORTD_BASE
-#define GPIO_BASE_SERVO	GPIO_PORTE_BASE
-#define M1 				GPIO_PA6_M1PWM2
-#define M2 				GPIO_PA7_M1PWM3
-#define M3 				GPIO_PD0_M1PWM0
-#define M4 				GPIO_PD1_M1PWM1
-#define SERVO1			GPIO_PE4_M0PWM4
-#define SERVO2			GPIO_PE5_M0PWM5
-// GPIO Defines - Control
-#define PD2_M1A			GPIO_PIN_2
-#define PD3_M1B			GPIO_PIN_3
-#define PE1_M2A			GPIO_PIN_1
-#define PE2_M2B			GPIO_PIN_2
-#define PE3_M3A			GPIO_PIN_3
-#define PA5_M3B			GPIO_PIN_5
-#define PB4_M4A			GPIO_PIN_4
-#define PB5_M4B			GPIO_PIN_5
-// SYSCTL Defines
-#define SYSCTL_MOTORS 	SYSCTL_PERIPH_PWM1
-#define SYSCTL_SERVOS 	SYSCTL_PERIPH_PWM0
-
+#include "motors.h"
 void Robot_PWM_init(void)
 {
 /*
@@ -132,10 +66,59 @@ void Robot_PWM_init(void)
 	GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, PA5_M3B);
 	GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, PB4_M4A | PB5_M4B);
 }
-//stoprobot
-//motorsfw
-//motorsrv
-//motorstl
-//motorstr
+void stop_motors(uint8_t duty)
+{
+	// Set all motors to brake
+	GPIOPinWrite(GPIO_PORTD_BASE, PD2_M1A | PD3_M1B, 0);
+	GPIOPinWrite(GPIO_PORTE_BASE, PE1_M2A | PE2_M2B | PE3_M3A, 0);
+	GPIOPinWrite(GPIO_PORTA_BASE, PA5_M3B, 0);
+	GPIOPinWrite(GPIO_PORTB_BASE, PB4_M4A | PB5_M4B, 0);
+}
+void coast_motors(uint8_t duty)
+{
+	// Set duty cycle to zero
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M1_OUT, 0);
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M2_OUT, 0);
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M3_OUT, 0);
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M4_OUT, 0);
+}
+void fw_motors(uint8_t duty)
+{
+	//M1:FWD, M2:REV, M3:FWD, M4:REV
+	//M1:FWD, 1A hi, 1B lo
+	GPIOPinWrite(GPIO_PORTD_BASE, PD2_M1A | PD3_M1B, PD2_M1A);
+	//M2:REV, 2A lo, 2B hi, M3:FWD, 3A hi
+	GPIOPinWrite(GPIO_PORTE_BASE, PE1_M2A | PE2_M2B | PE3_M3A, PE2_M2B | PE3_M3A);
+	//M3:FWD, 3B low
+	GPIOPinWrite(GPIO_PORTA_BASE, PA5_M3B, 0);
+	//M4:REV, 4A lo, 4B hi
+	GPIOPinWrite(GPIO_PORTB_BASE, PB4_M4A | PB5_M4B, PB5_M4B);
+}
+void rv_motors(uint8_t duty)
+{
+
+}
+void tl_motors(uint8_t duty)
+{
+
+}
+void tr_motors(uint8_t duty)
+{
+	// Set periods on Motors
+	uint32_t period = PWMGenPeriodGet(PWM_MOTOR_BASE, PWM_GEN_TOPM);
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M1_OUT, ((duty * period)/100));
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M2_OUT, ((duty * period)/100));
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M3_OUT, ((duty * period)/100));
+	PWMPulseWidthSet(PWM_MOTOR_BASE, M4_OUT, ((duty * period)/100));
+	//M1:FWD, M2:REV, M3:REV, M4:FWD
+	//M1:FWD, 1A hi, 1B lo
+	GPIOPinWrite(GPIO_PORTD_BASE, PD2_M1A | PD3_M1B, PD2_M1A);
+	//M2:REV, 2A lo, 2B hi, M3:REV, 3A lo
+	GPIOPinWrite(GPIO_PORTE_BASE, PE1_M2A | PE2_M2B | PE3_M3A, PE2_M2B);
+	//M3:REV, 3B hi
+	GPIOPinWrite(GPIO_PORTA_BASE, PA5_M3B, PA5_M3B);
+	//M4:FWD, 4A hi, 4B lo
+	GPIOPinWrite(GPIO_PORTB_BASE, PB4_M4A | PB5_M4B, PB4_M4A);
+}
 
 
---
+++ motors.h
@@ -0,0 +1,167 @@
+/*
+ * motors.h
+ *
+ *  Created on: Nov 26, 2014
+ *      Author: Wicho
+ */
+
+#ifndef MOTORS_H_
+#define MOTORS_H_
+/*
+ * Library Includes
+ */
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/* TIVAC Function Library */
+#include <driverlib/pin_map.h>
+#include <inc/hw_memmap.h>
+#include <driverlib/pwm.h>
+#include <driverlib/gpio.h>
+#include <driverlib/sysctl.h>
+//
+//		Robot
+//------------------
+//
+//	M1			M2
+//
+//	M3			M4
+//
+//------------------
+//
+//	Camera Servos
+// Pan: Servo 1
+// Tilt: Servo 2
+//------------------
+
+// Switching frequencies
+#define swfreq_motors 	50
+#define swfreq_servos 	30
+
+// PWM out Pins
+#define M1_OUT			PWM_OUT_2
+#define M2_OUT			PWM_OUT_3
+#define M3_OUT			PWM_OUT_0
+#define M4_OUT			PWM_OUT_1
+#define SERVO1_OUT		PWM_OUT_4
+#define SERVO2_OUT		PWM_OUT_5
+#define M1_OUT_BIT		PWM_OUT_2_BIT
+#define M2_OUT_BIT		PWM_OUT_3_BIT
+#define M3_OUT_BIT		PWM_OUT_0_BIT
+#define M4_OUT_BIT		PWM_OUT_1_BIT
+#define SERVO1_OUT_BIT	PWM_OUT_4_BIT
+#define SERVO2_OUT_BIT	PWM_OUT_5_BIT
+// PWM Gens
+#define PWM_MOTOR_BASE	PWM1_BASE
+#define PWM_SERVO_BASE	PWM0_BASE
+#define PWM_GEN_BOTM	PWM_GEN_0
+#define PWM_GEN_TOPM	PWM_GEN_1
+#define PWM_GEN_SERVO	PWM_GEN_2
+// GPIO Defines - Motor
+#define GPIO_M1 		GPIO_PIN_6
+#define GPIO_M2 		GPIO_PIN_7
+#define GPIO_M3 		GPIO_PIN_0
+#define GPIO_M4 		GPIO_PIN_1
+#define GPIO_SERVO1 	GPIO_PIN_4
+#define GPIO_SERVO2 	GPIO_PIN_5
+#define GPIO_BASE_TOPM	GPIO_PORTA_BASE
+#define GPIO_BASE_BOTM	GPIO_PORTD_BASE
+#define GPIO_BASE_SERVO	GPIO_PORTE_BASE
+#define M1 				GPIO_PA6_M1PWM2
+#define M2 				GPIO_PA7_M1PWM3
+#define M3 				GPIO_PD0_M1PWM0
+#define M4 				GPIO_PD1_M1PWM1
+#define SERVO1			GPIO_PE4_M0PWM4
+#define SERVO2			GPIO_PE5_M0PWM5
+// GPIO Defines - Control
+#define PD2_M1A			GPIO_PIN_2
+#define PD3_M1B			GPIO_PIN_3
+#define PE1_M2A			GPIO_PIN_1
+#define PE2_M2B			GPIO_PIN_2
+#define PE3_M3A			GPIO_PIN_3
+#define PA5_M3B			GPIO_PIN_5
+#define PB4_M4A			GPIO_PIN_4
+#define PB5_M4B			GPIO_PIN_5
+// SYSCTL Defines
+#define SYSCTL_MOTORS 	SYSCTL_PERIPH_PWM1
+#define SYSCTL_SERVOS 	SYSCTL_PERIPH_PWM0
+/*
+ * Function prototypes
+ */
+//*****************************************************************************
+//
+//! Moves the robot to the right
+//!
+//! \param ui8duty is the intended duty cycle
+//!
+//! Given a duty cycle, this function moves the robot via mecanum wheels,
+//! transationally right. This is done via force vectors.
+//!
+//! \return none
+//
+//*****************************************************************************
+void tr_motors(uint8_t duty);
+//*****************************************************************************
+//
+//! Moves the robot to the left
+//!
+//! \param ui8duty is the intended duty cycle
+//!
+//! Given a duty cycle, this function moves the robot via mecanum wheels,
+//! transationally right. This is done via force vectors.
+//!
+//! \return none
+//
+//*****************************************************************************
+void tl_motors(uint8_t duty);
+//*****************************************************************************
+//
+//! Moves the robot in the reverse direction
+//!
+//! \param ui8duty is the intended duty cycle
+//!
+//! Given a duty cycle, this function moves the robot backwards via mecanum wheels
+//!
+//! \return none
+//
+//*****************************************************************************
+void rv_motors(uint8_t duty);
+//*****************************************************************************
+//
+//! Moves the robot in the foward direction
+//!
+//! \param ui8duty is the intended duty cycle
+//!
+//! Given a duty cycle, this function moves the foward backwards via mecanum wheels
+//!
+//! \return none
+//
+//*****************************************************************************
+void fw_motors(uint8_t duty);
+//*****************************************************************************
+//
+//! Coast robots
+//!
+//! \param none
+//!
+//! This function allows the motors to coast by turning off the PWM signal
+//!
+//! \return none
+//
+//*****************************************************************************
+void coast_motors(uint8_t duty);
+//*****************************************************************************
+//
+//! Stop_motors
+//!
+//! \param none
+//!
+//! This function puts the motors in brake mode, causing the robot to cease movement
+//!
+//! \return none
+//
+//*****************************************************************************
+void stop_motors(uint8_t duty);
+#endif /* MOTORS_H_ */
