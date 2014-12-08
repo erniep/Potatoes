743a535edf08bf7342f5ca84bcedeb133d16cde6
erniep <theamericanflip@Yahoo.com>
2014-12-08 00:08:34 -0600

    Maze 5.1
    Added algo with Dennis's implementation rather than mine

--- Maze Navigation/Ernie's Ultra Simulator Function.c
+++ Maze Navigation/Ernie's Ultra Simulator Function.c
@@ -10,3 +10,160 @@ void naviSyst::callWallSensorsSimErnie() {
 
 	c->setWallScan();
 }
+
+if(n == 48){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 41){
+		c->setWall('N',true);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 40){
+		c->setWall('N',false);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 39){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',true);
+		c->setWall('W',true);	
+	}
+	else if(n == 38){
+		c->setWall('N',true);
+		c->setWall('E',true);
+		c->setWall('S',true);
+		c->setWall('W',false);
+	}
+	else if(n == 37){
+		c->setWall('N',false);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 34){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 33){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 32){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 31){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',false);
+	}
+	else if(n == 30){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',false);
+		c->setWall('W',true);
+	}
+	else if(n == 27){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 26){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 25){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',false);
+	}
+	else if(n == 24){
+		c->setWall('N',false);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',false);
+	}
+	else if(n == 23){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 20){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',true);
+	}
+	else if(n == 19){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',true);
+		c->setWall('W',false);
+	}
+	else if(n == 18){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 17){
+		c->setWall('N',false);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 16){
+		c->setWall('N',false);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',true);
+	}
+	else if(n == 13){
+		c->setWall('N',true);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',true);
+	}
+	else if(n == 12){
+		c->setWall('N',true);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',false);
+	}
+	else if(n == 11){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',true);
+		c->setWall('W',false);
+	}
+	else if(n == 10){
+		c->setWall('N',true);
+		c->setWall('E',false);
+		c->setWall('S',false);
+		c->setWall('W',true);
+	}
+	else if(n == 9){
+		c->setWall('N',true);
+		c->setWall('E',true);
+		c->setWall('S',false);
+		c->setWall('W',true);
+	}
